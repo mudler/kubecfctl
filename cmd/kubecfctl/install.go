614d3d1a51d86a96e4b1543e1ee1c3686d071787
Ettore Di Giacinto <edigiacinto@suse.com>
2020-10-27 18:25:44 +0100

    Rollback in case of deployment failure

--- cmd/kubecfctl/install.go
+++ cmd/kubecfctl/install.go
@@ -53,6 +53,11 @@ var installCmd = &cobra.Command{
 		err = inst.Install(kubecf, *cluster)
 		if err != nil {
 			fmt.Println(err)
+			err = inst.Delete(kubecf, *cluster)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
 			os.Exit(1)
 		}
 	},
